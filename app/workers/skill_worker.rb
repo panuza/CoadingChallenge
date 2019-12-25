9af69a7d5578e713b40dd19483bf2c5936ef89af
panuza <psp2049@gmail.com>
2019-12-25 21:19:27 +1100

    Add skill worker

---
+++ app/workers/skill_worker.rb
@@ -0,0 +1,10 @@
+class SkillWorker
+  include Sidekiq::Worker
+
+  def perform(user_id, answer_id)
+    user = User.find(user_id)
+    answer = Answer.find(answer_id)
+
+    user.update(skill_level: answer.get_upvotes.size)
+  end
+end
