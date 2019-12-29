cc62c7163fece99861cec1f16d53fd700d7395f0
panuza <psp2049@gmail.com>
2019-12-30 01:12:39 +1100

    Handle secret key base

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,7 +2,11 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      if Rails.env.production?
+        JWT.encode(payload, ENV['SECRET_TOKEN'])
+      else
+        JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      end
     end
 
     def decode(token)
