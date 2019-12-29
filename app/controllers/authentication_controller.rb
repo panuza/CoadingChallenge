b3051a6aab8c65f2adb10d440474272ef761da37
panuza <psp2049@gmail.com>
2019-12-29 19:18:38 +1100

    Skip authenticate before signing

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,4 +1,6 @@
 class AuthenticationController < ApplicationController
+  skip_before_action :authenticate_request
+
   def authenticate
     command = AuthenticateUser.call(params[:email], params[:password])
 
