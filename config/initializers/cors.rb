f7f41227ffe46901087307c64b41a73dcef90a93
panuza <psp2049@gmail.com>
2019-12-30 00:25:02 +1100

    Add frontend url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080'
+    origins 'https://enigmatic-fjord-43564.herokuapp.com'
 
     resource '*',
       headers: :any,
